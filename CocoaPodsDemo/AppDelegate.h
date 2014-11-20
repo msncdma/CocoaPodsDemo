433c49bf6cd6d545d130b85aaf4b099e6de825ef
song.he <hs@songhedeMac-mini-6.local>
2014-11-20 09:57:08 +0800

    Initial Commit

---
+++ CocoaPodsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CocoaPodsDemo
+//
+//  Created by song.he on 14/11/20.
+//  Copyright (c) 2014年 song.he. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CocoaPodsDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CocoaPodsDemo
+//
+//  Created by song.he on 14/11/20.
+//  Copyright (c) 2014年 song.he. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
